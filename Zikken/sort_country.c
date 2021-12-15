be3cd0ea6edbf69761c1a38830fbf4904ca26d66
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-12-16 08:47:26 +0900

    Update sort_country.c

--- Zikken/sort_country.c
+++ Zikken/sort_country.c
@@ -1,1 +1,12 @@
-
+void Write(STUDENT* argument){
+  int column_nuber=Count();
+  int i;
+  for(i=0;i<column_nuber;i++){
+    printf(" %s ",argument[i].name);
+    printf("金メダル:%d ",argument[i].gold);
+    printf("銀メダル:%d ",argument[i].silver);
+    printf("銅メダル:%d ",argument[i].blonde);
+    printf("\n");
+  }
+  
+}
